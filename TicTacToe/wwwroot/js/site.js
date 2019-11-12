83bbca55da3d329eca4e011fe43b9da38485ae6d
Nathan Faltermeier <nefalt2@gmail.com>
2019-11-12 14:59:29 -0600

    Work from class

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = 'X';
+
+var turnElement = document.getElementById("turn");
+turnElement.innerHTML = `Player ${currentTurn}'s turn`;
+
+function cellClicked(event) {
+	event.preventDefault();
+	event.target.innerText = currentTurn;
+	nextTurn();
+}
+
+function nextTurn() {
+	if (currentTurn === 'X')
+		currentTurn = 'O';
+	else
+		currentTurn = 'X';
+	turnElement.innerHTML = `Player ${currentTurn}'s turn`;
+}
+
+var squares = document.getElementsByClassName('square');
+for (var i = 0; i < squares.length; i += 1) {
+	squares[i].addEventListener("click", cellClicked);
+}
